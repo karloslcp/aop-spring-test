98dd5731956e60acdf76e09c84800511031552fc
Karlos Chita <karloslcp@gmail.com>
2020-09-09 12:03:14 -0500

    Added started files

---
+++ src/main/java/sv/com/oci/aopspringtest/AopSpringTestApplication.java
@@ -0,0 +1,13 @@
+package sv.com.oci.aopspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/com/oci/aopspringtest/AopSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.oci.aopspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
