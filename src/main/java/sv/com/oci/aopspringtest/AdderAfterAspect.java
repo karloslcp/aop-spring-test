24bed358313ccb22beeb45aabcef413624cea078
Karlos Chita <karloslcp@gmail.com>
2020-09-09 17:58:16 -0500

    Replace Configuration annotation with Component

--- src/main/java/sv/com/oci/aopspringtest/AdderAfterAspect.java
+++ src/main/java/sv/com/oci/aopspringtest/AdderAfterAspect.java
@@ -7,10 +7,10 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Aspect
-@Configuration
+@Component
 public class AdderAfterAspect
 {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
--- src/main/java/sv/com/oci/aopspringtest/AdderBeforeAspect.java
+++ src/main/java/sv/com/oci/aopspringtest/AdderBeforeAspect.java
@@ -5,10 +5,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Aspect
-@Configuration
+@Component
 public class AdderBeforeAspect
 {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
